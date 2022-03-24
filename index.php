3229ddcc3c2b808c650334b9654a779c91067b9d
Yusuf Rizal I <mkh.yusuf@gmail.com>
2022-03-24 09:05:46 +0700

    angka terbulang

--- index.php
+++ index.php
@@ -6,22 +6,23 @@
  <body>   
  <?php  
 
- $nominal = isset($_GET['nominal']) ? $_GET['nominal'] : "0";   
- if ($nominal)   
+ $angka = isset($_GET['angka']) ? $_GET['angka'] : "0";   
+ if ($angka)   
  {   
-     $uang = number_format($nominal, 0, ',','.') ."</br>";   
-     $terbilang = ucwords(''.Terbilang($nominal).'');   
+     $no = number_format($angka, 0, ',','.') ."</br>";   
+     $terbilang = ucwords(''.Terbilang($angka).'');   
  }   
  ?>  
 
  <hr>  
 
  <form action="" method="get">   
+  <div style="overflow-x:auto; background-color: red">
  <table width="100%" border="0">  
   
   <tr>  
    <td width="100">Angka</td>  
-   <td><input type="text" name="nominal" required></td>  
+   <td><input type="number" name="angka" required></td>  
   </tr>  
  
   <tr>  
@@ -31,19 +32,20 @@
  </table>  
  </form>  
    
- <?php if (!empty($nominal)) { ?>  
- <table width="800" border="0" cellpadding="4" cellspacing="0" style="border: 1px solid #000;">  
+ <?php if (!empty($angka)) { ?>  
+ <table width="800" border="0" cellpadding="4" cellspacing="0" style="border: 1px solid #000;background-color: white">  
  
  <tr>  
-   <td valign="top" > Sejumlah </td>  
-   <td valign="top" > : <?php echo $terbilang;?></td>  
+   <td valign="top" ><h3> Terbilang </h3></td>  
+   <td valign="top" ><h3> : <?php echo $terbilang;?></h3></td>  
  </tr>  
 
  <tr>  
-   <td valign="bottom"> <h3><?php echo $uang;?> </h3> </td>  
+   <td valign="bottom"> <h3>Angka : <?php echo $no;?> </h3> </td>  
    
  </tr>  
  </table>  
+</div>
   <?php } ?> 
  <style>  
  a { text-decoration: none; color: #0903E8; font-family: verdana; }  
